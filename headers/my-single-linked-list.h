e80d6f79dc9940761fbc466f9fabfd4373a467a1
Cory Stever <cstever@hotmail.com>
2024-09-23 13:02:33 -0500

    Header file created and added, ReadMe placeholder Added

---
+++ headers/my-single-linked-list.h
@@ -0,0 +1,45 @@
+#ifndef KIRI_SINGLE_LINK
+#define KIRI_SINGLE_LINK
+#include <iostream>
+
+using namespace std;
+// Header file to declare my Singly linked list and its functions that will be used for this assignment.
+
+
+template<typename Item_Type>
+class Singly_Linked_List {
+private:
+    struct Node
+    {
+        Item_Type data;
+        Node* next;
+
+        Node(const Item_Type& item_data, Node* next_node = nullptr) : data(item_data), next(next_node) {}
+    };
+
+    Node* head;
+    Node* tail;
+    size_t num_items;
+
+public:
+    Singly_Linked_List();
+    ~Singly_Linked_List();
+
+    void push_front(const Item_Type& item);
+    void push_back(const Item_Type& item);
+
+    void pop_front();
+    void pop_back();
+
+    const Item_Type& front() const;
+    const Item_Type& back() const;
+
+    bool empty() const;
+
+    void insert(size_t index, const Item_Type& item);
+    bool remove(size_t index);
+
+    size_t find(const Item_Type& item);
+};
+
+#endif
